d54c80bebbef35df86645ecb49c05008eeb22a20
Hannah <Hannah2520@gmail.com>
2025-04-27 02:18:09 -0400

    implemented 3 user stories

---
+++ app/src/main/java/edu/uga/cs/rideshareshawnhannah/LoginActivity.java
@@ -0,0 +1,65 @@
+package edu.uga.cs.rideshareshawnhannah;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailField, passwordField;
+    private Button loginButton;
+
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.editTextEmail);
+        passwordField = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailField.getText().toString().trim();
+                String password = passwordField.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish(); // don't allow back to login
+                                } else {
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
--- app/src/main/java/edu/uga/cs/rideshareshawnhannah/MainActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareshawnhannah/MainActivity.java
@@ -1,21 +1,14 @@
 package edu.uga.cs.rideshareshawnhannah;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,55 +16,46 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "SuperApp";
+
+    private static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
+    private TextView textView;
+    private Button logoutButton;
+    private DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        TextView textView = findViewById( R.id.textView );
+
+        textView = findViewById(R.id.textView);
+        logoutButton = findViewById(R.id.buttonLogout);
         mAuth = FirebaseAuth.getInstance();
-        String email = "dawg@mail.com";
-        String password = "password";
-        mAuth.signInWithEmailAndPassword( email, password )
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-// Sign in success
-                            Log.d( TAG, "signInWithEmail:success" );
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        }
-                        else {
-// If sign in fails
-                            Log.d( TAG, "signInWithEmail:failure", task.getException() );
-                        }
-                    }
-                });
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference( "message" );
-// Read from the database value for ”message”
-        myRef.addValueEventListener( new ValueEventListener() {
+        myRef = database.getReference("message");
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-// This method is called once, initially, and when data is updated
-                String message = dataSnapshot.getValue(String.class);
-                Log.d( TAG, "Read message: " + message );
-                textView.setText( message );
-            }
-            @Override
-            public void onCancelled( DatabaseError error ) {
-// Failed to read value
-                Log.d( TAG, "Failed to read value.", error.toException() );
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Read message: " + message);
+                textView.setText(message);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
